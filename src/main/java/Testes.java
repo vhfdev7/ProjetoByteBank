2a7b4b8d6c91b5e4f84970f8423d3bb126e02df2
unknown <vhugoferreira1996@gmail.com>
2023-08-30 17:46:58 -0300

    atualizando

---
+++ src/main/java/Testes.java
@@ -0,0 +1,12 @@
+import java.math.BigDecimal;
+import java.util.*;
+
+public class Testes {
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+}
---
+++ src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -0,0 +1,30 @@
+package br.com.alura.bytebank;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    public Connection recuperarConexao() {
+        try {
+            return createDataSource().getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+        private HikariDataSource createDataSource(){
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl("jdbc:mysql://localhost:3306/byte_bank");
+            config.setUsername("vtodev");
+            config.setPassword("AmOrA@2023");
+            config.setMaximumPoolSize(10);
+
+            return new HikariDataSource(config);
+        }
+
+    }
